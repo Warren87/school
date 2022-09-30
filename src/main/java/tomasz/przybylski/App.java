b8c89f06987847fd5011a1f30b533387edc0697d
tomaszprzybylski <tomasz.przybylski@intive.com>
2022-09-30 18:13:18 +0200

    Array

--- src/main/java/tomasz/przybylski/App.java
+++ src/main/java/tomasz/przybylski/App.java
@@ -3,14 +3,11 @@ package tomasz.przybylski;
 public class App {
 
     public static void main(String[] args) {
-        String[] years = {"2022/2023",
-                "2567/1000",
-                "2023/2024",
-                "LAST YEAR!!!"};
+        String[] years = {"2022/2023", "HBBBSAJHS"};
 
         for (int i = 0; i < years.length; i++) {
-            System.out.println(i);
-            printMessage(years[i]);
+            printMessage(i + " " + years[i]);
+            System.out.println("-----");
         }
     }
 
