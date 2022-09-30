1434fc5b91c9d029fa00670afe7f59e0f1ccee46
tomaszprzybylski <tomasz.przybylski@intive.com>
2022-09-30 18:13:18 +0200

    Variables and methods

---
+++ src/main/java/tomasz/przybylski/App.java
@@ -0,0 +1,17 @@
+package tomasz.przybylski;
+
+public class App {
+
+    public static void main(String[] args) {
+        printMessage("2022/2023");
+        printMessage("BABLALA");
+    }
+
+    static void printMessage(String year){
+
+        System.out.println("Witajcie uczniowie!");
+        System.out.println("W nowym roku " + year);
+        System.out.println("W roku " +  year + " będzie wesoło");
+    }
+
+}
