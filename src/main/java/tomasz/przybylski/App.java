075b2388dec882c3aa2526b43ff2cdd31b50c17f
tomaszprzybylski <tomasz.przybylski@intive.com>
2022-10-07 18:19:41 +0200

    Array test

--- src/main/java/tomasz/przybylski/App.java
+++ src/main/java/tomasz/przybylski/App.java
@@ -3,15 +3,22 @@ package tomasz.przybylski;
 public class App {
 
     public static void main(String[] args) {
-        printMessage("2022/2023");
-        printMessage("BABLALA");
+        String[] years = {"2022/2023",
+                "2567/1000",
+                "2023/2024",
+                "LAST YEAR!!!"};
+
+        for (int i = 0; i < years.length; i++) {
+            System.out.println(i);
+            printMessage(years[i]);
+        }
     }
 
-    static void printMessage(String year){
+    static void printMessage(String year) {
 
         System.out.println("Witajcie uczniowie!");
         System.out.println("W nowym roku " + year);
-        System.out.println("W roku " +  year + " będzie wesoło");
+        System.out.println("W roku " + year + " będzie wesoło");
     }
 
 }
