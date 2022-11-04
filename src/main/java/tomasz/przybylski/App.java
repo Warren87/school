79c32c5353675893b62852681f4e9f5a102610a9
tomaszprzybylski <tomasz.przybylski@intive.com>
2022-11-04 17:22:40 +0100

    Conditional and for

--- src/main/java/tomasz/przybylski/App.java
+++ src/main/java/tomasz/przybylski/App.java
@@ -3,19 +3,26 @@ package tomasz.przybylski;
 public class App {
 
     public static void main(String[] args) {
-        String[] years = {"2022/2023", "HBBBSAJHS"};
+        String[] years = {"2022/2023", "2033/2044", "LAST YEAR"};
 
-        for (int i = 0; i < years.length; i++) {
-            printMessage(i + " " + years[i]);
-            System.out.println("-----");
+        for (String each : years) {
+            printMessage(each);
+            System.out.println("---");
         }
+
     }
 
     static void printMessage(String year) {
 
-        System.out.println("Witajcie uczniowie!");
-        System.out.println("W nowym roku " + year);
-        System.out.println("W roku " + year + " będzie wesoło");
+        if("LAST YEAR".equals(year)){
+            System.out.println("To niestety ostatni rok");
+        } else {
+            System.out.println("Witajcie uczniowie!");
+            System.out.println("W nowym roku " + year);
+            System.out.println("W roku " + year + " będzie wesoło");
+        }
+
+
     }
 
 }
