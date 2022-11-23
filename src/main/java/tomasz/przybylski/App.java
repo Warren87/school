792ef78a07eccaed3811a731e69fbd587553ade3
tomaszprzybylski <tomasz.przybylski@intive.com>
2022-11-23 17:57:45 +0100

    Objects first time

--- src/main/java/tomasz/przybylski/App.java
+++ src/main/java/tomasz/przybylski/App.java
@@ -3,6 +3,17 @@ package tomasz.przybylski;
 public class App {
 
     public static void main(String[] args) {
+        School school = new School();
+        System.out.println(school.name);
+
+        school.name = "Haha zmieniłem wam nazwę szkoły!";
+
+        System.out.println(school.name);
+
+        if (school.hasCantine) {
+            System.out.println("Szkoła ma stołówkę");
+        }
+
         String[] years = {"2022/2023", "2033/2044", "LAST YEAR"};
 
         for (String each : years) {
@@ -14,7 +25,7 @@ public class App {
 
     static void printMessage(String year) {
 
-        if("LAST YEAR".equals(year)){
+        if ("LAST YEAR".equals(year)) {
             System.out.println("To niestety ostatni rok");
         } else {
             System.out.println("Witajcie uczniowie!");
---
+++ src/main/java/tomasz/przybylski/School.java
@@ -0,0 +1,13 @@
+package tomasz.przybylski;
+
+public class School {
+
+    String name = "Szkoła im. Tomasz Przybylskiego";
+
+    int classCount = 20;
+
+    boolean hasCantine = false;
+
+    double averageGrade = 4.5;
+
+}
